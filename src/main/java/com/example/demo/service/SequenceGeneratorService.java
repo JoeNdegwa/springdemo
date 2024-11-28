e17675214107a3b852dbfb1dc41e61b1758d6cb8
Joseph Ndegwa <bulljoseph8@gmail.com>
2024-11-28 11:43:36 +0300

    Fix random ID generated

--- src/main/java/com/example/demo/service/SequenceGeneratorService.java
+++ src/main/java/com/example/demo/service/SequenceGeneratorService.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
 import static org.springframework.data.mongodb.core.query.Query.query;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 
@@ -15,10 +18,13 @@ public class SequenceGeneratorService {
     private MongoOperations mongoOperations;
 
     public long generateSequence(String seqName) {
+        Random randomObject = new Random();
+        int generatedId = randomObject.nextInt(89) * 887;
+
         DatabaseSequence counter = mongoOperations.findAndModify(
                 query(where("_id").is(seqName)),
-                new Update().inc("seq", 1),
+                new Update().inc("seq", generatedId),
                 DatabaseSequence.class);
-        return counter != null ? counter.getSeq() : 1;
+        return counter != null ? counter.getSeq() : generatedId;
     }
 }
